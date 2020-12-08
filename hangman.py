3118ef2524d5c283f1591a4241f523c99cab146b
Aman <amythapa3032@gmail.com>
2020-12-08 23:08:01 +0530

    initial interface

---
+++ hangman.py
@@ -0,0 +1,14 @@
+import random
+
+
+def hangman():
+    word = random.choice["bugger", "tiger", "superman", "thor", "pokemon", "earth", "water", "avengers"]
+
+
+
+
+name = input("Enter your name ")
+print("Welcome", name)
+print("---------------------")
+print("Try to guess the word in less than 10 attempts")
+hangman()
